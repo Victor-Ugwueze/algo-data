6f50427f58f61494e5244116be3dd478cb31a1c8
victor <victorugwueze@gmail.com>
2019-11-04 19:07:44 +0100

    add two linked list
    - add two linked list to return the sum as a single linked list

--- javascript/linkedList.js
+++ javascript/linkedList.js
@@ -8,19 +8,52 @@ function LinkedList() {
   this.head = null;
 }
 
-const linkedList = new LinkedList();
+function createList(c, b, a) {
+  const l1 = new LinkedList();
 
-linkedList.head = new Node(1);
+  l1.head = new Node(a);
 
-const second = new Node(2);
+  const second = new Node(b);
 
-const third = new Node(3);
+  const third = new Node(c);
 
-linkedList.head.next = second;
+  l1.head.next = second;
 
-second.next = third;
-
-while(linkedList.head) {
-  console.log(linkedList.head.data);
-  linkedList.head = linkedList.head.next;
+  second.next = third;
+  return l1;
 }
+
+const addTwoLinkedList = function(l1, l2) {
+  let dummyHead = new LinkedList();
+  let p = l1;
+  let q = l2;
+  let carry = 0;
+  let curr = dummyHead;
+
+  while(p.head || q.head) {
+    let x = p.head ? p.head.data : 0;
+    let y = q.head ? q.head.data : 0;
+    let sum = x + y + carry;
+    carry = Number.parseInt(sum / 10);
+    if(!dummyHead.head) {
+      curr.head = new Node(sum % 10);
+      curr = curr.head;
+    } else {
+      curr.next = new Node(sum % 10);
+      curr = curr.next;
+    }
+    p.head = p.head ? p.head.next : null;
+    q.head = q.head ? q.head.next : null;
+  }
+  if (carry > 0) {
+    curr.next = new Node(carry);
+  }
+  console.log(JSON.stringify(dummyHead))
+  return dummyHead;
+};
+
+const l1 = createList(4,6,5);
+const l2 = createList(3,4,2);
+
+
+addTwoLinkedList(l1, l2);
